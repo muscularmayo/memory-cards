cfee598778b15a8aba58c6016f78b420ff4f48ba
Chris Risden <christopherrisden@gmail.com>
2022-04-12 17:39:06 -0700

    just a little fix, making good progress - next step is to make the game logic and score logic and tie it all together, especially with the shuffle

--- src/MemoryCardGrid.js
+++ src/MemoryCardGrid.js
@@ -8,24 +8,6 @@ export default function MemoryCardGrid (props) {
   // [andy, angela, cathy, charles, clark, creed, darryl, david, deangelo, dwight, erin, gabe, hank, holly, jan, jim, jo, karen,
   //  kelly, kevin, meredith, nate, nellie, oscar, pam, pete, phyllis, robert, roy, ryan, stanley, toby, todd]
 
-  function shuffle (array) {
-    let currentIndex = array.length,  randomIndex;
-
-    // While there remain elements to shuffle.
-    while (currentIndex !== 0) {
-
-      // Pick a remaining element.
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-
-      // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
-    }
-
-    return array;
-  }
-
   const display = props.cardDisplay.map((obj, index) => {
     return (
     <MemoryCard src={obj.image} name={obj.name} key={index} />
